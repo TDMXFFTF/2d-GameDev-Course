788a23254a3c69a5a56fdb3767d2aee9c151a547
TDMXFFTF <159198325+TDMXFFTF@users.noreply.github.com>
2024-03-18 14:43:36 -0700

    L5 Making the ship move smoothly with steering

--- 04.to_space_and_beyond_workbook/practices/L4.P1.boosting_circle/boosting_circle.gd
+++ 04.to_space_and_beyond_workbook/practices/L4.P1.boosting_circle/boosting_circle.gd
@@ -4,7 +4,7 @@ var boost_speed := 1200.0
 var normal_speed := 600.0
 var max_speed := normal_speed
 var velocity := Vector2(0, 0)
-
+var steering_factor := 10.0
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
@@ -19,7 +19,9 @@ func _process(delta: float) -> void:
 		get_node("Timer").start()
 		pass
 
-	velocity = direction * max_speed
+	var desired_velocity := max_speed * direction 
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
