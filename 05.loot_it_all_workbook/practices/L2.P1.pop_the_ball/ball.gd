edbf26f58f48358c4c0e476dab868935716ce792
TDMXFFTF <159198325+TDMXFFTF@users.noreply.github.com>
2024-03-29 14:58:13 -0700

    L2 Your First Collectible

--- 05.loot_it_all_workbook/practices/L2.P1.pop_the_ball/ball.gd
+++ 05.loot_it_all_workbook/practices/L2.P1.pop_the_ball/ball.gd
@@ -3,3 +3,15 @@ extends Area2D
 
 func _ready() -> void:
 	pass
+
+
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	queue_free()
+
+
+func _on_ball_2_area_entered(area_that_entered: Area2D) -> void:
+	queue_free()
+
+
+func _on_ball_3_area_entered(area_that_entered: Area2D) -> void:
+	queue_free()
